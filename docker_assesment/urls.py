76a522d3dfd3673dbca76690391df7e826039cd5
EswarPriya T <eswarpriya@EswarPriyas-MacBook-Pro.local>
2019-11-15 18:47:55 +0530

    Initial Commit

---
+++ docker_assesment/urls.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from django.conf.urls import include,url
+from employer.views import (EmployeeList,DepartmentList,DepartmentCreate,
+    EmployeeCreate,EmployeeDepList,EmployeeRelation,Employeedetails,EmployeeUpdate,
+    EmployeeInfo)
+
+urlpatterns = [
+    url(r'^admin/',include(admin.site.urls)),
+    # url(r'^docs/',include(rest_framework_swagger.urls)),
+    url(r'^employee/list/$',EmployeeList.as_view()),
+    url(r'^employee/create/$',EmployeeCreate.as_view()),
+    url(r'^employee/update/(?P<pk>\d+)/$',EmployeeUpdate.as_view()),
+    url(r'^employee/info/$',EmployeeInfo.as_view()),
+    url(r'^department/list/$',DepartmentList.as_view()),
+    url(r'^department/employee/list/$', EmployeeDepList.as_view()),
+    url(r'^department/create/$',DepartmentCreate.as_view()),
+    url(r'^employee/relation/$',EmployeeRelation.as_view()),
+    url(r'^employee/details/$',Employeedetails.as_view())
+
+    # Here add your URL's
+]
